e0b85301f1ce527f33d7c76aecaafe6055e16151
MadaliN <madalinmen28@gmail.com>
2017-03-02 11:00:45 +0200

    Update class.tcl

--- Wildman/class.tcl
+++ Wildman/class.tcl
@@ -14,11 +14,11 @@ proc class:msgm {nick uhost hand arg} {
 	
 	switch -exact -- [lindex [split $arg] 0] {
 		class {
-			if {![file exists $class(path)$class(file)]} { putquick "PRIVMSG $nick :\002$nick\002 - Fisierul nu exista"; return }
+			if {![file exists $class(path)[lindex [split $arg] 1]]} { putquick "PRIVMSG $nick :\002$nick\002 - Fisierul nu exista"; return }
 
-			set class(file) [lindex [split $arg] 1]
 			set class(chan) [lindex [split $arg] 2]
-			set class(delay) [lindex [split $arg] 3]			
+			set class(delay) [lindex [split $arg] 3]	
+			set class(file) [lindex [split $arg] 1]
 			set class(start) 1
 			set class(end) 0
 			set class(pause) 0
@@ -26,6 +26,10 @@ proc class:msgm {nick uhost hand arg} {
 			set class(number) 0
 			set class(total) 0
 
+			if {$class(file) eq "" } { putquick "PRIVMSG $nick :\002::\002 Nu ai specificat \00304fisierul\003 pe care trebuie sa il rulez"; return }			
+			if {$class(chan) eq "" } { putquick "PRIVMSG $nick :\002::\002 Nu ai specificat \00304canalul\003 pe care sa rulez"; return }
+			if {$class(delay) eq "" } { putquick "PRIVMSG $nick :\002::\002 Nu ai specificat \00304timpul"; return }
+			
 			set of [open $class(path)$class(file)]; while {[gets $of line] ne -1} { incr class(total) }; close $of
 
 			putlog "Total linii: \00312$class(total)"
@@ -33,7 +37,7 @@ proc class:msgm {nick uhost hand arg} {
 			foreach c [channels] { if {[channel get $c class]} { set chan $c } }
 
 			putquick "PRIVMSG $nick :\002$nick\002 - Pornesc fisierul \00303$class(file)\003 pe canalul \00304$class(chan)\003 cu un delay de \00312$class(delay)\003 secunde"
-			class:start $chan $nick $class(delay)
+			class:start $chan $nick $class(delay) $class(file)
 		}
 		chanset {
 			channel set [lindex [split $arg] 1] [lindex [split $arg] 2]
@@ -43,31 +47,31 @@ proc class:msgm {nick uhost hand arg} {
 		stop {
 			set class(end) 1
 
-			killutimer $class(running)
-
 			putquick "PRIVMSG $nick :\002::\002 Am oprit cu succes"
+			
+			catch {killutimer $class(running)}
 		}
 		pause {
 			set class(pause) 1
 
-			killutimer $class(running)
-
 			putquick "PRIVMSG $nick :\002::\002 Am oprit temporar..."
+			
+			catch {killutimer $class(running)}
 		}
 		resume {
 			set class(pause) 0
 
-			class:start $class(chan) $nick $class(delay)
+			class:start $class(chan) $nick $class(delay) $class(file)
 			
 			putquick "PRIVMSG $nick :\002::\002 Pornim de unde am ramas..."
 		}
 	}
 }
 
-proc class:start {chan nick delay} {
+proc class:start {chan nick delay file} {
 	global class
 
-	putlog "$chan $nick $delay"
+	putlog "$chan $nick $delay ($class(path)$file)"
 	
 	if {$class(pause)} { putlog "gasit pauza"; return }
 	if {$class(end)} { putlog "gasit oprit"; return }
@@ -75,7 +79,7 @@ proc class:start {chan nick delay} {
 
 	incr class(number)
 
-	set of [open $class(path)$class(file)]
+	set of [open $class(path)$file]
 
 	while {[gets $of line] ne -1} {
 		incr class(line)
@@ -87,5 +91,5 @@ proc class:start {chan nick delay} {
 	set class(line) 0
 	close $of
 
-	set class(running) [utimer $delay [list class:start $chan $nick $class(delay)]]
+	set class(running) [utimer $delay [list class:start $chan $nick $class(delay) $file]]
 }
